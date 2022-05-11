6ead3d7b0d4a7674ea79c0c954f31c5adbb97ca9
redcoder54 <redcoder54@gmail.com>
2022-05-11 12:11:22 +0800

    delete unused shortcut-key-handler classes

--- src/main/java/redcoder/texteditor/shortcut/ShortcutKeyHandler.java
+++
@@ -1,9 +0,0 @@
-package redcoder.texteditor.shortcut;
-
-import java.awt.event.KeyEvent;
-import java.util.Set;
-
-public interface ShortcutKeyHandler {
-
-    boolean handle(KeyEvent e, Set<Integer> pressedKeys);
-}
