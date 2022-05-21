588586d0ea988c242c5f90c6369fc45e95c4a811
redcoder54 <redcoder54@gmail.com>
2022-05-21 16:51:53 +0800

    add maven shade plugins

--- src/main/java/redcoder/texteditor/resources/IconResource.java
+++ src/main/java/redcoder/texteditor/resources/IconResource.java
@@ -7,7 +7,6 @@ import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -42,46 +41,55 @@ public class IconResource {
 
     private static void loadDefaultIcon() {
         try {
-            loadImageFromLocalFile();
-            loadImageFromJarFile();
+            loadImageIcon("images/icons");
+            loadImageIcon("toolbarButtonGraphics/general");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void loadImageFromLocalFile() throws Exception {
+    private static void loadImageIcon(String path) throws Exception {
         ClassLoader classLoader = IconResource.class.getClassLoader();
-        URL url = classLoader.getResource("images/icons");
+        URL url = classLoader.getResource(path);
         if (url == null) {
             return;
         }
+        String str = url.toString();
+        if (str.startsWith("file")) {
+            loadLocalFile(url);
+        } else if (str.startsWith("jar")) {
+            loadJarFile(classLoader, url);
+        } else {
+            System.err.println("Unknown URL[ " + str + "], we can't handle it.");
+        }
+    }
+
+    private static void loadLocalFile(URL url) throws Exception {
         File file = new File(url.toURI());
         File[] files = file.listFiles();
         if (files != null) {
             for (File f : files) {
-                String iconName = f.getName();
-                ICONS.put(iconName, new ImageIcon(f.getAbsolutePath()));
+                if (f.isFile()) {
+                    String iconName = f.getName();
+                    String path = f.getAbsolutePath();
+                    ICONS.put(iconName, new ImageIcon(path));
+                }
             }
         }
     }
 
-    private static void loadImageFromJarFile() throws Exception {
-        ClassLoader classLoader = IconResource.class.getClassLoader();
-        URL url = classLoader.getResource("toolbarButtonGraphics");
-        if (url == null) {
-            return;
-        }
+    private static void loadJarFile(ClassLoader classLoader, URL url) throws Exception {
         JarURLConnection connection = (JarURLConnection) url.openConnection();
         JarFile jarFile = connection.getJarFile();
         Enumeration<JarEntry> entries = jarFile.entries();
         while (entries.hasMoreElements()) {
             String name = entries.nextElement().getName();
-            if (name.endsWith(".gif")) {
-                Optional.ofNullable(classLoader.getResource(name))
-                        .ifPresent(icon -> {
-                            String iconName = name.substring(name.lastIndexOf("/") + 1);
-                            ICONS.put(iconName, new ImageIcon(icon));
-                        });
+            if (name.endsWith(".gif") || name.endsWith(".png")) {
+                URL resource = classLoader.getResource(name);
+                if (resource != null) {
+                    String iconName = name.substring(name.lastIndexOf("/") + 1);
+                    ICONS.put(iconName, new ImageIcon(resource));
+                }
             }
         }
     }
